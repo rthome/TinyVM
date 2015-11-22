24a009012a9dda39a6b4b4469178abbd64abbe82
Raffael Thome <mail@rmt.name>
2015-11-22 14:27:47 +0100

    Begin to implement a parser in the assembler

--- tasm.py
+++ tasm.py
@@ -7,7 +7,14 @@
 
 import sys, argparse, re
 
-class Token(object):
+# # #
+# Tokenization
+# # #
+
+class TokRule(object):
+    DISCARD_TXT = 1
+    DISCARD_TOK = 2
+
     def __init__(self, name, rule, flags=()):
         self.name = name
         self.rule = re.compile(rule)
@@ -17,28 +24,27 @@ class Token(object):
     def tuple(self):
         return self.name, self.rule, self.flags
 
-TOK_DISCARD_TXT = 1
-TOK_DISCARD_TOK = 2
 TOK_RULES = [
-    Token("newline", r"\r\n|\n", flags=(TOK_DISCARD_TXT,)),
-    Token("whitespace", r"\s+", flags=(TOK_DISCARD_TOK,)),
-    Token("comment", r";[^\n]*", flags=(TOK_DISCARD_TOK,)),
-    Token("label", r"\w+:"),
-    Token("label_ref", r":\w+"),
-    Token("specifier", r"\.\w+"),
-    Token("literal", r"#\d+"),
-    Token("brk_open", r"\["),
-    Token("brk_close", r"\]"),
-    Token("number", r"\d+"), # TODO: Support more number types
-    Token("register", r"r([0-9]+|IP|IC|SP|SBP|RMD)"),
-    Token("identifier", r"\w+"),
+    TokRule("newline", r"\r\n|\n", flags=(TokRule.DISCARD_TXT,)),
+    TokRule("whitespace", r"\s+", flags=(TokRule.DISCARD_TOK,)),
+    TokRule("comment", r";[^\n]*", flags=(TokRule.DISCARD_TOK,)),
+    TokRule("label", r"\w+:"),
+    TokRule("label_ref", r":\w+"),
+    TokRule("specifier", r"\.\w+"),
+    TokRule("literal", r"#\d+"),
+    TokRule("brk_open", r"\["),
+    TokRule("brk_close", r"\]"),
+    TokRule("number", r"\d+"), # TODO: Support more number types
+    TokRule("register", r"r([0-9]+|IP|IC|SP|SBP|RMD)"),
+    TokRule("identifier", r"\w+"),
 ]
 
 def tokenize(string):
-    def process(tok, text, flags):
-        if TOK_DISCARD_TXT in flags:
+    "Break up an input string into a bunch of (token, text) tuples"
+    def process_flags(tok, text, flags):
+        if TokRule.DISCARD_TXT in flags:
             text = ""
-        if TOK_DISCARD_TOK in flags:
+        if TokRule.DISCARD_TOK in flags:
             tok = None
             text = ""
         return tok, text
@@ -53,7 +59,7 @@ def tokenize(string):
                 start, end = match.span()
                 if start > last_end:
                     yield "unknown", string[last_end:start]
-                ptok, ptext = process(tok, match.group(), flags)
+                ptok, ptext = process_flags(tok, match.group(), flags)
                 if ptok is not None:
                     yield ptok, ptext
                 last_end = pos = match.end()
@@ -62,6 +68,43 @@ def tokenize(string):
             pos += 1
     if last_end < len(string):
         yield "unknown", string[last_end:]
+
+# # #
+# Parsing
+# # #
+
+def extract_texts(tokens):
+    return [txt for (tok, txt) in tokens]
+
+def parse_operands(op_tokens):
+    "Parse a list of tokens into a list of instruction operands"
+    return extract_texts(op_tokens)
+
+def parse_line(line):
+    """Parse a line of tokens into a label, a specifier, or a instruction
+    Returns tuples of (type, name, [operands])"""
+    (tok, text), *rest = line
+    if tok == "label":
+        return "label", text[:-1]
+    elif tok == "specifier":
+        return "specifier", text[1:], extract_texts(rest)
+    elif tok == "identifier":
+        return "instruction", text, parse_operands(rest)
+
+def parse(tokens):
+    "Parse a bunch of tokens into instructions, labels, and specifiers"
+    line = []
+    for tok, text in tokens:
+        if tok != "newline":
+            line.append((tok, text))
+            continue
+        if len(line) > 0:
+            yield parse_line(line)
+            line.clear()
+
+# # #
+# Glue
+# # #
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="The TinyVM Assembler")
@@ -72,8 +115,11 @@ def parse_arguments():
 def main():
     args = parse_arguments()
     with open(args.file) as f:
-        for tok, text in tokenize(f.read()):
+        tokens = list(tokenize(f.read()))
+        for tok, text in tokens:
             print(tok + ": " + text)
+        for x in parse(tokens):
+            print(x)
 
 if __name__ == "__main__":
     main()
